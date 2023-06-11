229506f1ecc33e234cf3c99424e1ac362b0abfaa
Peysone <peysone@MacBook-Pro-Peysone.local>
2023-06-11 18:27:45 +0200

    poprawione rozwiązanie

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 """
 Stwórz program, który pozwoli wczytać (z klawiatury) imię psa i jego datę urodzenia. Zapisz dane w postaci słownika.
 """
-while True:
-    doggo_name = {str(input("podaj imie psiurka: "))}
-    doggo_birthdate = {str(input("podaj date urodzenia psiurka: "))}
-    if doggo_name or doggo_birthdate == "q":
-       break
+# while True:
+#     doggo_name = {str(input("podaj imie psiurka: "))}
+#     doggo_birthdate = {str(input("podaj date urodzenia psiurka: "))}
+#     dict_of_doggos = [doggo_name, doggo_birthdate]
+#     if doggo_name or doggo_birthdate == "q":
+#         break
+#
+#
+# print(100 * "-")
+# print(dict_of_doggos)
 
-dict_of_doggos = [doggo_name, doggo_birthdate]
-print(100 * "-")
+dict_of_doggos = {}
+for i in range(5):
+    doggo_name = str(input("Podaj imię psiurka: "))
+    doggo_birthdate = int(input("Podaj ile ma lat: "))
+
+    dict_of_doggos[doggo_name] = doggo_birthdate
+
 print(dict_of_doggos)
